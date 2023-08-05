05d82679a28f1aa2c23fe52f2ebe47a332b10681
Jaanuja Koneswaran <106996073+JaaNithu@users.noreply.github.com>
2023-08-05 17:08:56 +0530

    Add files via upload

---
+++ Blog page/resources/views/post/index.blade.php
@@ -0,0 +1,20 @@
+<?php
+/** @var $posts \Illuminate\Pagination\LengthAwarePaginator */
+?>
+
+
+
+ <x-app-layout :meta-title="'Mind Bolg - Post by category' . $category->title" meta-description="Bycategory description">
+ <!-- Posts Section -->
+ <section class="w-full md:w-2/3 flex flex-col items-center px-3">
+
+    @foreach ($posts as $post)
+        <x-post-item :post="$post"></x-post-item>
+    @endforeach
+
+    {{$posts->onEachSide(1)->links()}}
+
+    </section>
+    <x-sidebar />
+
+</x-app-layout>
