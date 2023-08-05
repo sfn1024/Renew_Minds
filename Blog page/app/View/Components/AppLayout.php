49cc99e0f425c96ea28205bdebf90462641a3594
Jaanuja Koneswaran <106996073+JaaNithu@users.noreply.github.com>
2023-08-05 17:01:19 +0530

    Add files via upload

---
+++ Blog page/app/View/Components/AppLayout.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use App\Models\Category;
+use Illuminate\View\Component;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\View\View;
+
+class AppLayout extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(public ?string $metaTitle = null, public ?string $metaDescription = null)
+    {
+
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        $query = Category::query()
+            ->join('category_post', 'categories.id', '=', 'category_post.category_id')
+            ->select('categories.id', 'categories.title', 'categories.slug', DB::raw('count(*) as total'))
+            ->groupBy('categories.id', 'categories.title', 'categories.slug')
+            ->orderByDesc('total')
+            ->limit(5);
+
+        $categories =$query
+            ->get();
+        return view('layouts.app', compact('categories'));
+    }
+}
