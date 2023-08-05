7d07ab68a9f0495ce12489c765cf138cb4f45f78
Jaanuja Koneswaran <106996073+JaaNithu@users.noreply.github.com>
2023-08-05 17:32:17 +0530

    Add files via upload

---
+++ Blog page/storage/views/52186e93334a7c1afdb5c16d0316a1ff.php
@@ -0,0 +1,8 @@
+<td
+    <?php echo e($attributes->class(['filament-tables-reorder-cell w-4 whitespace-nowrap px-4'])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</td>
+<?php /**PATH /var/www/html/vendor/filament/tables/src/../resources/views/components/reorder/cell.blade.php ENDPATH**/ ?>
